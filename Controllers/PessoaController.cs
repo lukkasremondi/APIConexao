ce2dcbbb6528b9151516fb47313cc729ce0901ad
lukkasremondi <lukkasremondi@hotmail.com>
2021-12-27 10:51:24 -0300

    Criacao Rota Post

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -2,13 +2,33 @@ using System.Threading.Tasks;
 using APIConexao.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using APIConexao.Models;
 
 namespace APIConexao.Controllers
 {
     [Controller]
     [Route("[controller]")]
-    public class PessoaController
+    public class PessoaController : ControllerBase
     {
+
+        private DataContext dc;
+
+        public PessoaController(DataContext context){
+
+            this.dc = context;
+        }
+
+        [HttpPost("api")]
+
+        public async Task<ActionResult> cadastrar([FromBody] Pessoa p){
+
+            dc.pessoa.Add(p);
+            await dc.SaveChangesAsync();
+
+            return Created("Objeto Pessoa", p);
+        }
+
+
         [HttpGet("oi")]
         public string oi(){
             return "Hello World";
