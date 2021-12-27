634dc522204c80874e068889d4c79c74950aa975
lukkasremondi <lukkasremondi@hotmail.com>
2021-12-27 10:05:39 -0300

    Ajustes e criacao da Model

---
+++ Models/Pessoa.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace APIConexao.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome {get;set;}
+
+        public string Cidade {get;set;}
+
+        public int Idade {get;set;}
+        
+    }
+}
